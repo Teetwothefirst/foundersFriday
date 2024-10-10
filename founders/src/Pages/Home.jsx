a1de649511d38d3241d188e00ca858f8631951a7
Teetwothefirst <dexterdavid835@gmail.com>
2024-10-10 10:18:25 -0700

    Doing: Added the footer reuseable component

--- founders/src/Pages/Home.jsx
+++ founders/src/Pages/Home.jsx
@@ -1,8 +1,13 @@
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.jpg'
+import Footer from '../component/footer'
 function Home(){
     return <>
-    <header className="flex">
+    <header className="">
         <div><img src={logo} alt="Founders Friday Logo" /></div>
     </header>
+
+    <Footer></Footer>
     </>
 }
+
+export default Home
