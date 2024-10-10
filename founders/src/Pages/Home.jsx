2a308702e71a896c92fcbf2c76699d3076fcf672
Teetwothefirst <dexterdavid835@gmail.com>
2024-10-10 09:36:32 -0700

    Initial Local Commit

---
+++ founders/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/logo.png'
+function Home(){
+    return <>
+    <header className="flex">
+        <div><img src={logo} alt="Founders Friday Logo" /></div>
+    </header>
+    </>
+}
