26c0198cf15b458099d6c02047eee99093856abf
Teetwothefirst <dexterdavid835@gmail.com>
2024-10-11 14:08:46 -0700

    Edit to the main section

--- founders/note.md
+++ founders/note.md
@@ -3,3 +3,10 @@
 Change the Style of the Static Picture of the Profile image to actual Images with the styling
 Change the image of the learning section to a Video
 
+# Docs
+
+
+
+# Learning Point
+
+Don't 
--- founders/src/component/mainSection.jsx
+++ founders/src/component/mainSection.jsx
@@ -1,23 +1,69 @@
 import profile from '../assets/profiler.jpg'
 import videoImage from '../assets/videoImg.png'
+import lineJustAboveTestimonial from '../assets/lineJustAboveTestimonial.png'
+import bgImg from '../assets/bg-img.png'
+import logo from '../assets/logo1.png'
 
 function mainSection(){
         const names = ['Mr Belba Ngoy', 'Mr Belba Ngoy', 'Mr Belba Ngoy']
   
     return <>
     <div>
+        <div>
+            <div className='relative'>
+                <img src={bgImg} alt="" width={`100%`}/>
+                <div className='absolute top-24 text-white'>
+                    <div className='px-5 md: flex justify-between gap-5'>
+                        <div>
+                            <h3>Founders Friday is coming to</h3>
+                            <h1 className='text-violet-700'>Kaduna</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac <br /> rutrum felis. 
+                                Nulla nibh lorem, facilisis vel est at, vehicula dignissim <br /> lacus. Cras sagittis imperdiet dolor, 
+                                eget accumsan leo suscipit id. <br />Maecenas ut ante quis quam lobortis consequat eu id turpis. 
+                                Pellentesque habitant morbi tristique senectus et <br />netus et malesuada fames ac turpis egestas
+                            </p>
+                        </div>
+                        <div>
+                            <img src={logo} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        {/* Roadmap */}
+        <div className="px-5 my-10">
+            <div>
+                <h3>Register And Be Part of Our Community</h3>
+                <p>Join our community of over 1000+ founders, developers, and tech junkies in general.Be inspired by people who live to inspire!</p>
+            </div>
+            <div>
+                <img src={lineJustAboveTestimonial} alt="" width={`100%`}/>
+            </div>
+            <div className='my-10'>
+                <button className="px-14 bg-white text-violet-700 py-2">Register Now</button>
+            </div>
+        </div>
+
+
         {/* Video Proof */}
-        <div className="md:flex justify-between px-5">
+        <div className="md:flex justify-between px-5 gap-5">
             <div>
                 <img src={videoImage} alt="" />
             </div>
-            <div>
+            <div className='flex justify-center flex-col'>
+                
                 <strong>At Founder’s Friday, Every Friday Is a Learning Experience!</strong>
                 <p>Join us in our mission to transform ideas into impact and shape the future of Nigeria’s startup landscape.</p>
+                <div>
                 <button className="bg-violet-700 text-white py-2 px-8 border rounded-full">Register <i class="bi bi-arrow-right"></i></button>
+                </div>
+                
             </div>
         </div>
         
+
         {/* Testimonials Section */}
         <div className="md:flex justify-center">
             <div>
@@ -44,8 +90,10 @@ function mainSection(){
             </div>
 
         </div>
+
+
         {/* FAQs Section */}
-        <div className='my-10'>
+        <div className='my-10 md:px-10'>
             <h3 className="">FAQs</h3>
             <div className="px-11 md: flex justify-evenly gap-11">
                 <div>Lorem ipsum dolor sit amet, consectetur adi elit.</div>
